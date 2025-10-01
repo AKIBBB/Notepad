41d53da60981a21c6a81d1f96c5cd4060aacefc6
Monirul Hoque AKib <aaakibmonir@gmail.com>
2025-10-02 03:07:16 +0600

    Add new feature / fix bug / update note page

--- notepad/settings.py
+++ notepad/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4!-csgch3-1*7)91a!r+d-ozd7ke17eq(+mk%aryk$bt84u*dr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = "/media/"
 # settings.py
